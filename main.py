69b511a600b7046dc787772072b9050bca7e53df
roberto-saliola <roberto.saliola@accenture.com>
2026-03-05 15:29:35 +0100

    accept user prompt from args

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 model = "gemini-2.5-flash"
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 if api_key is None:
     raise RuntimeError("Put your api_key in .env")
 
 def main():
-    res_from_ai = client.models.generate_content(contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.", model=model)
+    res_from_ai = client.models.generate_content(contents=args.user_prompt, model=model)
    
     if res_from_ai.usage_metadata == None:
         raise RuntimeError("No usage metadata in ai response")
