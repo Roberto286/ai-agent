b470cdaa04d1c1035fb74c72c8773f59af7615db
Roberto Saliola <roberto.saliola96@gmail.com>
2026-03-11 17:05:37 +0100

    added LLM loop
    fixed some logic in available_functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 import argparse
@@ -20,36 +21,60 @@ args = parser.parse_args()
 if api_key is None:
     raise RuntimeError("Put your api_key in .env")
 
+
 def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    res_from_ai = client.models.generate_content(contents=messages, model=model, config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[available_functions ]))
-   
-    if res_from_ai.usage_metadata == None:
-        raise RuntimeError("No usage metadata in ai response")
-    
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {res_from_ai.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: { res_from_ai.usage_metadata.candidates_token_count }")
+    for _ in range(20):
+        res_from_ai = client.models.generate_content(
+            contents=messages,
+            model=model,
+            config=types.GenerateContentConfig(
+                system_instruction=system_prompt, tools=[available_functions]
+            ),
+        )
 
-    if res_from_ai.function_calls:
-        print(res_from_ai.function_calls)
-        for fc in res_from_ai.function_calls:
-            function_call_result = call_function(fc, args.verbose)
+        if res_from_ai.candidates:
+            for candidate in res_from_ai.candidates:
+                if candidate.content:
+                    messages.append(candidate.content)
 
-            if not function_call_result.parts:
-                raise Exception("No valid parts in function_call_result")
-            
-            if not function_call_result.parts[0].function_response:
-                raise Exception("function_response is None") 
+        if not res_from_ai.usage_metadata:
+            raise RuntimeError("No usage metadata in ai response")
 
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception("response is None")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {res_from_ai.usage_metadata.prompt_token_count}")
+            print(
+                f"Response tokens: {res_from_ai.usage_metadata.candidates_token_count}"
+            )
 
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(res_from_ai.text)
+        if not res_from_ai.function_calls:
+            print("Response:")
+            print(res_from_ai.text)
+            return
+
+        if res_from_ai.function_calls:
+            print(res_from_ai.function_calls)
+            function_responses = []
+
+            for function_call in res_from_ai.function_calls:
+                result = call_function(function_call, args.verbose)
+                if (
+                    not result.parts
+                    or not result.parts[0].function_response
+                    or not result.parts[0].function_response.response
+                ):
+                    raise RuntimeError(
+                        f"Empty function response for {function_call.name}"
+                    )
+                if args.verbose:
+                    print(f"-> {result.parts[0].function_response.response}")
+                function_responses.append(result.parts[0])
+
+            messages.append(types.Content(role="user", parts=function_responses))
+
+    sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
